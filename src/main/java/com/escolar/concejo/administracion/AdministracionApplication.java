4b4062d0dd70f59c81f633483e92a005dd5e213c
nocampo-technisys <nocampo@technisys.com>
2019-08-08 20:35:37 -0300

    add configuration properties and annotations

--- src/main/java/com/escolar/concejo/administracion/AdministracionApplication.java
+++ src/main/java/com/escolar/concejo/administracion/AdministracionApplication.java
@@ -2,8 +2,10 @@ package com.escolar.concejo.administracion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AdministracionApplication {
 
 	public static void main(String[] args) {
