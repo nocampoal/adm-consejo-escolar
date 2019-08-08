1ce754f046a17364af26b0a162176239ec33524f
nocampo-technisys <nocampo@technisys.com>
2019-08-08 17:51:31 -0300

    first commit

---
+++ src/main/java/com/escolar/concejo/administracion/AdministracionApplication.java
@@ -0,0 +1,13 @@
+package com.escolar.concejo.administracion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministracionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministracionApplication.class, args);
+	}
+
+}
