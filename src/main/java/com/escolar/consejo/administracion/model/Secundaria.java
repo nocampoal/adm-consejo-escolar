f40806f937458d1bc97fce0137a98ad47c3f3c86
Nicolas Ocampo <nicolas.ocampo@globant.com>
2020-03-21 15:23:23 -0300

    add lombook dependency for getter and setters

--- src/main/java/com/escolar/consejo/administracion/model/Secundaria.java
+++ src/main/java/com/escolar/consejo/administracion/model/Secundaria.java
@@ -2,32 +2,14 @@ package com.escolar.consejo.administracion.model;
 
 import javax.persistence.Entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
+@Getter
+@Setter
 public class Secundaria extends Escuela {
 	private Long cupoCena;
 	private String tipoJornada;   //simple doble completa
 	
-	
-	
-	public Secundaria() {
-		super();	
-	}
-	public Long getCupoCena() {
-		return cupoCena;
-	}
-	public void setCupoCena(Long cupoCena) {
-		this.cupoCena = cupoCena;
-	}
-	public String getTipoJornada() {
-		return tipoJornada;
-	}
-	public void setTipoJornada(String tipoJornada) {
-		this.tipoJornada = tipoJornada;
-	}
-	
-	
-	
-	
-	
-
 }
